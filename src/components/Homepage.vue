d2590c7be4721e56c5908ab995f4edd8f12771c1
Breno Henrique <brenosc2@hotmail.com>
2017-09-30 11:24:48 -0300

    setup bootstrap-vue

---
+++ src/components/Homepage.vue
@@ -0,0 +1,26 @@
+<template>
+  <b-container class="bv-example-row">
+    <b-row>
+      <b-col col md="4">1 of 3</b-col>
+      <b-col col md="4">2 of 3</b-col>
+      <b-col col md="4">3 of 3</b-col>
+    </b-row>
+  </b-container>
+</template>
+
+
+<script>
+export default {
+  name: 'homepage',
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+// Styles
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+// Core Libs
 import Vue from 'vue';
+
+// Externals
+import BootstrapVue from 'bootstrap-vue';
+
 import App from './App';
 import router from './router';
+
+Vue.use(BootstrapVue);
 
 Vue.config.productionTip = false;
 
